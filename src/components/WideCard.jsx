c9306b630d71e2687fb9ff009aadffe22c77f4d6
SV60 <dakisdrop@gmail.com>
2025-02-05 17:49:46 +0100

    Update WideCard.jsx peliswatchlistv1

--- src/components/WideCard.jsx
+++ src/components/WideCard.jsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 WideCard.propTypes = {
-  item: PropTypes.string.isRequired,
-  onRemove: PropTypes.string.isRequired,
+  item: PropTypes.object.isRequired, // Cambié a 'object' ya que 'item' es un objeto
+  onRemove: PropTypes.func.isRequired, // Cambié a 'func' ya que 'onRemove' es una función
 };
 
 export default function WideCard(props) {
@@ -16,18 +16,31 @@ export default function WideCard(props) {
   };
 
   return (
-      <Link to={`/info/${props.item.type}/${props.item.item.id}`} className="widecard flex relative w-full h-full overflow-hidden rounded-lg group">
-        <img className="widecard-image group-hover:scale-105 w-[21.5vw] max-2xl:w-[29vw] max-xl:w-[43.2vw] max-md:w-full h-full object-cover transition-all duration-150" src={props.item.item.backdrop_path && `https://image.tmdb.org/t/p/w500/${props.item.item.backdrop_path}`} alt="Backdrop" />
-        <div className="widecard-content group flex justify-start items-end absolute w-full h-full bottom-0 left-0 bg-gradient-to-t from-black">
-          <button className='flex justify-center items-center absolute top-1 right-1 opacity-0 w-9 h-9 bg-white/90 border-0 p-0 rounded-lg cursor-pointer transition-all duration-150 group-hover:opacity-100 max-2xl:opacity-100 hover:bg-red-500' onClick={(event) => removeItem(event, props.item.id)}>
-            <i className="fa-light fa-trash-can text-lg text-black" alt="Remove" />
-          </button>
-          <div className='p-2'>
-            <div className='font-light text-[0.8rem]'>{props.item.type === 'tv' ? `Watching S${props.item.season} Episode-${props.item.episode}` : ''}</div>
-            <p className='font-medium text-[0.9rem]'>{props.item.type === 'movie' ? props.item.item.title : props.item.item.name}</p>
+    <Link to={`/info/${props.item.type}/${props.item.item.id}`} className="widecard flex relative w-full h-full overflow-hidden rounded-lg group">
+      <img 
+        className="widecard-image group-hover:scale-105 w-[21.5vw] max-2xl:w-[29vw] max-xl:w-[43.2vw] max-md:w-full h-full object-cover transition-all duration-150"
+        src={props.item.item.backdrop_path && `https://image.tmdb.org/t/p/w500/${props.item.item.backdrop_path}`} 
+        alt="Backdrop" 
+      />
+      <div className="widecard-content group flex justify-start items-end absolute w-full h-full bottom-0 left-0 bg-gradient-to-t from-black">
+        <button 
+          className='flex justify-center items-center absolute top-1 right-1 opacity-0 w-9 h-9 bg-white/90 border-0 p-0 rounded-lg cursor-pointer transition-all duration-150 group-hover:opacity-100 max-2xl:opacity-100 hover:bg-red-500' 
+          onClick={(event) => removeItem(event, props.item.id)}
+        >
+          <i className="fa-light fa-trash-can text-lg text-black" alt="Remove" />
+        </button>
+        <div className='p-2'>
+          <div className='font-light text-[0.8rem]'>
+            {props.item.type === 'tv' ? `Watching S${props.item.season} Episode-${props.item.episode}` : ''}
           </div>
-          <div className="widecard-play flex items-center absolute bottom-5 right-[10px]"><i className="fa-solid fa-play fa-xl text-white" alt="Play Icon" /></div>
+          <p className='font-medium text-[0.9rem]'>
+            {props.item.type === 'movie' ? props.item.item?.title : props.item.item?.name}
+          </p>
         </div>
-      </Link>
+        <div className="widecard-play flex items-center absolute bottom-5 right-[10px]">
+          <i className="fa-solid fa-play fa-xl text-white" alt="Play Icon" />
+        </div>
+      </div>
+    </Link>
   );
 }
